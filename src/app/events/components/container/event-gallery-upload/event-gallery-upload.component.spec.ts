0b4aa5483a8504b34292f5be284a115739b0c3f0
Gustavo Costa <gustavo.costa.w@gmail.com>
2021-05-24 00:23:41 -0300

    update to angular 12

--- src/app/events/components/container/event-gallery-upload/event-gallery-upload.component.spec.ts
+++ src/app/events/components/container/event-gallery-upload/event-gallery-upload.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EventGalleryUploadComponent } from './event-gallery-upload.component';
 
@@ -6,7 +6,7 @@ describe('EventGalleryUploadComponent', () => {
   let component: EventGalleryUploadComponent;
   let fixture: ComponentFixture<EventGalleryUploadComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EventGalleryUploadComponent ]
     })
--- src/app/events/components/presentation/event-dialog/event-dialog.component.spec.ts
+++ src/app/events/components/presentation/event-dialog/event-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EventDialogComponent } from './event-dialog.component';
 
@@ -6,7 +6,7 @@ describe('EventDialogComponent', () => {
   let component: EventDialogComponent;
   let fixture: ComponentFixture<EventDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EventDialogComponent ]
     })
